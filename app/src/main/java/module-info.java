e1c3338dce07667b67750c13b329a98db90d5371
ilias <118639812+iliaselamrani212@users.noreply.github.com>
2023-02-02 20:07:39 +0100

    vrai

--- app/src/main/java/module-info.java
+++ app/src/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.example {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
-
+requires transitive  javafx.graphics ;
     opens com.example to javafx.fxml;
     opens com.example.controllers to javafx.fxml;
     exports com.example;
