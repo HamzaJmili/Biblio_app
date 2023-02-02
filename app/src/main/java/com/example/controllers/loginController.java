82c1f91b575a2b71f6016440a5729fb73a156dba
ilias <118639812+iliaselamrani212@users.noreply.github.com>
2023-02-02 17:52:45 +0100

    add eye to login page

--- app/src/main/java/com/example/controllers/loginController.java
+++ app/src/main/java/com/example/controllers/loginController.java
@@ -1,12 +1,67 @@
 package com.example.controllers;
+import java.io.IOException;
+
+import com.example.App;
+
+import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 
-public class loginController {
+import javafx.scene.Group;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class loginController  {
+    private static boolean visible = false ;
+   
+        @FXML
+        private Button closeButton;
+        @FXML
+        private PasswordField password_password_field;
+        @FXML
+        private TextField password_text_field ;
+        @FXML
+        private Button eye_button_passwordfield ;
+       
     @FXML
-    private void test(ActionEvent event){
-        System.out.println("ilias");
+    protected void initialize (){
+        password_text_field.setVisible(false);
+         
     }
+        
+    
+        @FXML
+        void closeButtonClicked(ActionEvent event) throws IOException {
+            
+            ((Stage) closeButton.getScene().getWindow()).close();
+            
+           
+        }
+        
+        @FXML
+        void eye_button(ActionEvent event )throws IOException{
+            if (visible==false){
+                
+                password_text_field.setText(password_password_field.getText());
+                password_password_field.setVisible(false);
+                password_text_field.setVisible(true);
+                visible=true;
+                return ;
+            }
+            if(visible==true){
+                password_password_field.setText(password_text_field.getText());
+                password_text_field.setVisible(false);
+                password_password_field.setVisible(true);
+                visible=false;
+                return;
+               
+            }
+        }
+
 
     
 }
