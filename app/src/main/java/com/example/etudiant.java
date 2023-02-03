785341075d55be7cd91cfbf3c3a0aa688e9cead6
younes20044 <youn.elamrani@edu.umi.ac.ma>
2023-02-03 10:30:52 +0100

    projet

--- app/src/main/java/com/example/etudiant.java
+++ app/src/main/java/com/example/etudiant.java
@@ -1,60 +1,16 @@
 package com.example;
 
-public class Etudiant {
+public class Etudiant extends User {
     private String cne ;
-    private String nom_etudiant ;
-    private String prenom_etudiant ;
-    private String email_etudiant ;
-    private String mdp_etudiant ;
-
-    public Etudiant(String cne, String nom_etudiant, String prenom_etudiant, String email_etudiant,String mdp_etudiant) {
-        this.cne = cne;
-        this.nom_etudiant = nom_etudiant;
-        this.prenom_etudiant = prenom_etudiant;
-        this.email_etudiant = email_etudiant;
-        this.mdp_etudiant = mdp_etudiant;
+    
+    public Etudiant(String cne,String nom_user, String prenom_user, String email_user, String mdp_user) {
+        super(nom_user, prenom_user, email_user, mdp_user);
+        this.cne=cne;
     }
-
     public String getCne() {
         return cne;
     }
-
     public void setCne(String cne) {
         this.cne = cne;
     }
-
-    public String getNom_etudiant() {
-        return nom_etudiant;
-    }
-
-    public void setNom_etudiant(String nom_etudiant) {
-        this.nom_etudiant = nom_etudiant;
-    }
-    
-    public String getPrenom_etudiant() {
-        return prenom_etudiant;
-    }
-
-    public void setPrenom_etudiant(String prenom_etudiant) {
-        this.prenom_etudiant = prenom_etudiant;
-    }
-
-    public String getEmail_etudiant() {
-        return email_etudiant;
-    }
-
-    public void setEmail_etudiant(String email_etudiant) {
-        this.email_etudiant = email_etudiant;
-    }
-
-    public String getMdp_etudiant() {
-        return mdp_etudiant;
-    }
-
-    public void setMdp_etudiant(String mdp_etudiant) {
-        this.mdp_etudiant = mdp_etudiant;
-    }
-    
-
-
 }
