411fdcebf54154f6b041e5155046bd63d0240477
younes20044 <youn.elamrani@edu.umi.ac.ma>
2023-02-03 12:48:24 +0100

    projet

--- app/src/main/java/com/example/modele_admin.java
+++ app/src/main/java/com/example/modele_admin.java
@@ -6,13 +6,24 @@ import java.sql.Statement;
 
 
 public  class Modele_admin {
-    public static User checkadmin(String email, String mdp) throws SQLException{
-   ResultSet rs= conn.getStatement().executeQuery("select * from admin where email='"+email+"' and mdp_admin='"+mdp+"' ");
- if(rs.wasNull()) 
- return null ;
-     User adm = new Admin(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5));
-   return adm;
+  public static User checkEtudiant(String email,String mot_de_pass) {
+    System.out.println("222");
+    try {
+      Statement s=conn.getStatement();      
+      ResultSet rs= conn.getStatement().executeQuery("select * from admin where email='"+email+"' and mdp_admin='"+mot_de_pass+"' ");
+      
+        while (rs.next()) {
+          User adm = new Admin(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5));
+          return adm;
+        }
+        return null;
+            
+      } catch (SQLException e) {
+        System.out.println("mmmmmmm");
+        return null;
         
-    }
+      }
+ 
     
+}
 }
