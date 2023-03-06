23ce1cfe269f7fc4ba3b9a9bccdf56cf2d183cf6
younes20044 <youn.elamrani@edu.umi.ac.ma>
2023-03-06 13:05:52 +0100

    hhhh

--- app/src/main/java/com/example/controllers/leftbarEtudiantController.java
+++ app/src/main/java/com/example/controllers/leftbarEtudiantController.java
@@ -9,12 +9,13 @@ import com.example.Auteur;
 import com.example.Livre;
 import com.example.Modele_auteur;
 import com.example.Modele_livre;
-
+import com.example.Session;
 
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -31,6 +32,9 @@ import javafx.scene.paint.Paint;
 import javafx.scene.shape.Line;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class leftbarEtudiantController {
     @FXML
@@ -40,6 +44,43 @@ public class leftbarEtudiantController {
        
      
          App.setRoot(Parametres.getScene(),"param");
+}
+
+@FXML
+void gotolivre() throws IOException{
+    
+    App.setRoot(Parametres.getScene(),"ListOfBooksStudent");
+}
+
+@FXML
+public void gotocmpt() throws IOException{
+    Stage stage2 =new Stage();
+    Scene scene2 = new Scene(App.loadFXML("inform"));
+      stage2.setScene(scene2);
+      stage2.setResizable(false);
+      stage2.initModality(Modality.APPLICATION_MODAL);
+      stage2.show();
+  }
+@FXML
+void decon() throws IOException{
+    Session.email_utiliasteur=null;
+    Session.id_utiliasteur=null;
+    Session.nom_utiliasteur=null;
+    Session.prenom_utiliasteur=null;
+    ((Stage)  Parametres.getScene().getWindow()).close();
+    Stage stage2 =new Stage();
+    Scene scene2 = new Scene(App.loadFXML("login_inter"));
+      stage2.setScene(scene2);
+      stage2.setResizable(false);
+      stage2.initStyle(StageStyle.UNDECORATED);
+      stage2.show();
+   
+}
+@FXML
+void gotoaide() throws IOException{
+       
+     
+    App.setRoot(Parametres.getScene(),"aide");
 }
 }
 
